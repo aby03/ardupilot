09b1bb4a4523fe24aa019a91a68e11fcd8c9a1e8
aby03 <mt6160648@iitd.ac.in>
2020-06-27 00:58:54 +0530

    fixed

--- ArduCopter/mode_new_control.cpp
+++ ArduCopter/mode_new_control.cpp
@@ -16,8 +16,7 @@
 // COMMANDS: To set roll pitch
 // channel_roll->set_control_in(<float value>);   channel_pitch->set_control_in(<float value>);   channel_yaw->set_control_in(<float value>);
 	
-const AP_InertialNav& inav;
-const AP_InertialNav& _inav;
+
 
 
 
@@ -67,6 +66,11 @@ float accel_lim = 250.0f;
 float pid_accel_max = accel_lim;
 float pid_accel_min = -accel_lim;
 float _dt = 0.02;
+
+float throttle_in;
+float pid_accel;
+float pid_vel_max;
+float pid_vel_min;
 //pos_control->set_alt_target(target_z); // Line not able to call
 // Altitude target set
 float target_z = 1000.0f; //HARD CODED 
@@ -136,8 +140,8 @@ bool ModeNewControl::init(bool ignore_checks)
 		error_max[i] = 400;
 		prev_error[i] = 0;
 	}
-	float pid_vel_max = 250;
-	float pid_vel_min = -250;
+	pid_vel_max = 250;
+	pid_vel_min = -250;
 	// pid_max[pitch_i] = 400;
 	// pid_max[yaw_rate_i] = 400;
 	
@@ -440,17 +444,17 @@ void ModeNewControl::update_motors()
 
 void ModeNewControl::throttle_control()
 {
-    curr_alt = _inav.get_altitude();
+    curr_alt = inertial_nav.get_altitude();
     
     // clear position limit flags
-    _limit.pos_up = false;
-    _limit.pos_down = false;
+    // _limit.pos_up = false;
+    // _limit.pos_down = false;
 
     // calculate altitude error
     error_z = target_z - curr_alt;
     // calculate _vel_target.z using from _pos_error.z using sqrt controller
     _vel_target = AC_AttitudeControl::sqrt_controller(error_z, THR_ALT_P, accel_lim, _dt);
-    const Vector3f&  _vel_current = _inav.get_velocity();
+    const Vector3f&  _vel_current = inertial_nav.get_velocity();
     error_vel = _vel_target - _vel_current.z ;
     error_sum_vel += error_vel ;
 // To limit the ki value
@@ -473,8 +477,8 @@ void ModeNewControl::throttle_control()
 	if(pid_vel < pid_vel_min)
 	{	pid_vel = pid_vel_min;	}
 	
-	accel_target = (pid_vel-cur_vel)/_dt;
-	accel_cur = -(_ahrs.get_accel_ef_blended().z + GRAVITY_MSS) * 100.0f;
+	accel_target = (pid_vel-_vel_current.z)/_dt;
+	accel_cur = -(ahrs.get_accel_ef_blended().z + GRAVITY_MSS) * 100.0f;
 	error_accel = accel_target -  accel_cur;
 	error_sum_accel += error_accel;
 // To limit the ki value for acceleration
@@ -497,15 +501,21 @@ void ModeNewControl::throttle_control()
 	if(pid_accel < pid_accel_min)
 	{	pid_accel = pid_accel_min;	}
 	
-	thr_out =  _motors.get_throttle_hover() + pid_accel;
+	thr_out =  motors->get_throttle_hover() + pid_accel;
 
 	if(thr_out>1)
 	{	thr_out = 1;	}
 	if(thr_out<0)
 	{	thr_out = 0;	}
 
-	throttle_in = get_throttle_boosted(throttle_in);
-	_motors.set_throttle(throttle_in);
+	// throttle_in = get_throttle_boosted(thr_out);
+	// get throttle boosted code copied below
+    float cos_tilt = ahrs.cos_pitch() * ahrs.cos_roll();
+    float inverted_factor = constrain_float(2.0f * cos_tilt, 0.0f, 1.0f);
+    float boost_factor = 1.0f / constrain_float(cos_tilt, 0.5f, 1.0f);
+    float throttle_out = thr_out * inverted_factor * boost_factor;
+	// get throttle boosted end
+	motors->set_throttle(throttle_out);
 	
 
 
